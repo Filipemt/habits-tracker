1748c97f01633c236bc046cf0b34cd35074147a4
Filipe Mota <filipera3@gmail.com>
2025-11-26 18:04:49 -0300

    feat: init components header/footer/layout

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-white border-t-2 p-4">
+  <p class="text-center">
+    Criado por Filipe Mota. O código fonte está no <a href="https://github.com/Filipemt/habits-tracker" class="underline">GitHub.</a>
+  </p>
+</footer>
